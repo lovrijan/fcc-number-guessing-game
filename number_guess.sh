e30f67a16e3b1bf5d703949bed845ef876270ea9
lovrijan <92103513+lovrijan@users.noreply.github.com>
2024-08-27 10:12:17 +0200

    Create number_guess.sh

---
+++ number_guess.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
+BEST_PLAY=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
+
+#check if username available
+if [[ -z $USERNAME_AVAIL ]]
+  then
+    INPUT_USER=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_PLAY guesses."
+fi
+
+
+RANDOM_NUMBER=$((1 + $RANDOM % 1000))
+GUESS=1
+echo "Guess the secret number between 1 and 1000:"
+while read NUM
+do
+  if [[ ! $NUM =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+    else 
+      if [[ $NUM -eq $RANDOM_NUMBER ]] #if guess is equal to random number
+      then
+        break;
+      else
+        if [[ $NUM -gt $RANDOM_NUMBER ]] #if guess is greater than random num
+        then
+          echo -n "It's lower than that, guess again:"
+        elif [[ $NUM -lt $RANDOM_NUMBER ]] #if guess is lower than random num
+          then
+            echo -n "It's higher than that, guess again:"
+        fi
+      fi
+  fi
+  GUESS=$(( $GUESS + 1 ))
+done
+
+if [[ $GUESS == 1 ]]
+  then
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
+  else
+    echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
+fi
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES ($GUESS, $USER_ID)")
